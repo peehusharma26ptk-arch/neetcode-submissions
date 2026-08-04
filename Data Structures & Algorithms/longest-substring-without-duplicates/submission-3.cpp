50bcc6175e3477adaef3018b6b1f0eb5d3906964
peehusharma26ptk-arch <peehusharma26ptk@gmail.com>
2026-08-04 19:42:44 +0530

    Add: longest-substring-without-duplicates - submission-3

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int j=0;
+        int maxi=0;
+        unordered_map<char,int> mp;
+        for(int i=0;i<s.length();i++){
+            mp[s[i]]++;
+            while(mp[s[i]]>1){
+               
+                    mp[s[j]]--;
+                
+                j++;
+            }
+            maxi=max(maxi,i-j+1);
+        }
+        return maxi;
+    }
+};
